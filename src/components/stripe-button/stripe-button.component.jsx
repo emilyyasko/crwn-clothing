f270adc08220a0451139a2f9c3a614dc92e58c31
Emily Yaskowitz <emily@Emilys-MacBook-Pro.local>
2020-06-29 10:55:34 -0700

    Updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51GzQqAKEwThV8I1XEdiyQOHGFzZ5RBN1OoeK87IN0SW80bQteeLm5XVGNclV6fMhk4Pza1Paq5bwlmIwEBIaWNa400hJFOqZND';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
